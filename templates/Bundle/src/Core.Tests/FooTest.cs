129fa43a888385060bb0b0795bfebffb532eb11b
Rodrigo B. de Oliveira <rbo@acm.org>
2012-11-14 10:35:01 -0200

    bundle template

---
+++ templates/Bundle/src/Core.Tests/FooTest.cs
@@ -0,0 +1,15 @@
+using Core;
+using NUnit.Framework;
+
+namespace Core.Tests
+{
+	[TestFixture]
+	public class FooTest
+	{
+		[Test]
+		public void Bar()
+		{
+			new Foo().Bar();
+		}
+	}
+}
---
+++ templates/Bundle/src/Core/Foo.cs
@@ -0,0 +1,10 @@
+namespace Core
+{
+	public class Foo
+	{
+		public void Bar()
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
